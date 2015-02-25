5a44cf1b3f84bdf11f0bfef93ca870f97aabb3c5
mindaugaskadzys <mindaugas.kadzys@gmail.com>
2015-02-25 14:32:37 +0000

    Pick up items
    Added staging scene for item pickup, currently destroys item when picked
    up

---
+++ Assets/Scripts/Interact.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interact : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 fwd = transform.TransformDirection (Vector3.forward);
+		RaycastHit hitObject;
+		if (Physics.Raycast (transform.position, fwd, out hitObject, 1)) {
+			if(Input.GetKey("e") && hitObject.collider.CompareTag("Item")) {
+				//TODO: put item in inventory
+				Destroy (hitObject.collider.gameObject);
+			}
+			else
+				print ("Unknown item");
+		}
+	}
+}
