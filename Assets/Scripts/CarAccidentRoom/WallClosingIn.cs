e992c0b8dd5848961e2f0a9b0ddf3f123e779ad2
Tamara Tanasescu <gokimster@yahoo.com>
2015-02-25 15:18:03 +0000

    Walls prefab and script - not finished

---
+++ Assets/Scripts/CarAccidentRoom/WallClosingIn.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallClosingIn : MonoBehaviour {
+	
+	public float speed = 1;
+	public float moveDistance = 3;
+	Vector3 initialPos;
+	public Vector3 direction = Vector3.left;
+	// Use this for initialization
+	void Start () {
+		Physics.IgnoreLayerCollision (15, 15);
+		initialPos = rigidbody.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!(Vector3.Distance(initialPos, rigidbody.position) >= moveDistance))
+			rigidbody.velocity = direction * speed;
+		else
+			rigidbody.velocity = new Vector3(0,0,0);
+
+	}
+}
