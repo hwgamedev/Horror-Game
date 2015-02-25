91a77a3ee6e73f2ccdbf7747de5f0d940f74f699
Tamara Tanasescu <gokimster@yahoo.com>
2015-02-25 16:57:34 +0000

    Start of flickering lights ...

---
+++ Assets/Scripts/LightScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightScript : MonoBehaviour {
+
+	public float minFlickerSpeed = 0.1f; 
+	public float maxFlickeringSpeed = 1;
+	float initialTime;
+	public bool lightEnabled = true;
+	float waitTime;
+	private Light light1;
+
+	void Start () {
+		light1 = GetComponent<Light> ().light;
+		initialTime = Time.time;
+		waitTime = Random.Range (minFlickerSpeed, maxFlickeringSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((Time.time - initialTime) >= waitTime) {
+			Flicker ();
+			initialTime = Time.time;
+			waitTime = Random.Range (minFlickerSpeed, maxFlickeringSpeed);
+		}
+	}
+
+	void Flicker()
+	{
+		//light1.intensity = Random.Range (0,1.1f);
+		light1.enabled = !lightEnabled;
+		lightEnabled = !lightEnabled;
+	}
+}
