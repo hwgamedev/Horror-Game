2759320e1ded9ace71dc58f042de30070abd930a
plebras <pl196.hw@gmail.com>
2015-03-11 13:42:26 +0000

    finished table

---
+++ Assets/Scripts/CarAccidentRoom/wardrobe.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wardrobe : MonoBehaviour {
+
+	public Rigidbody doorL;
+	public Rigidbody doorR;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		doorL.transform.Rotate(0,30,0);
+	}
+
+
+}
